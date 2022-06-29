7ede3ac121e748d35d74d330ee1ef9a96928e0ce
Hasnain Merchant <hasnainmerchant0@gmail.com>
2022-06-30 01:55:19 +0530

    Revert "Working with JSON Example of Doctor Appointment (Create Patient)"
    This reverts commit cbd59dec2901d20efe2b5b5661d12d9c621cd92c.

--- JSON_CSV/Doctor_Appointment.py
+++ JSON_CSV/Doctor_Appointment.py
@@ -1,4 +1,3 @@
-# Doctor Clinic (USING JSON)
 import json
 
 class Patient:
@@ -16,24 +15,14 @@ class Patient:
         elif self.choice == 3:
             self.update_patient()
 
-    def save_data(self):
-        print("[+] SAVING TO FILE....")
-        with open("patient_data.json", "w") as patient_data:
-            json.dump(self.patient_data, patient_data, indent=8)
-
     def display_data(self):
-        for p in self.patient_data["patient_list"]:
-            print(f"Patient's Id - {p['patient_id']}")
-            print(f"Patient's Firstname - {p['firstname']}")
-            print(f"Patient's Lastname - {p['lastname']}")
-            print(f"Patient's Gender - {p['gender']}")
-            print(f"Patient's Age - {p['age']}")
-            print("\n\n")
+        print(self.patient_data)
 
     def create_patient(self):
         print("[+] CREATING PATIENT....")
 
         patient = {}
+
         patient["patient_id"] = int(input("ENTER PATIENT ID - "))
         patient["firstname"] = input("ENTER PATIENT's FIRSTNAME - ")
         patient["lastname"] = input("ENTER PATIENT's LASTNAME - ")
@@ -48,26 +37,17 @@ class Patient:
 
         self.display_data()
 
+    def save_data(self):
+        print("[+] SAVING TO FILE....")
+        with open("patient_data.json", "w") as patient_data:
+            json.dump(self.patient_data, patient_data, indent=8)
+            print("[+] FILE UPDATED")
+
     def delete_patient(self):
         print("[-] DELETING PATIENT....")
-        self.display_data()
-        patient_id = int(input("ENTER PATIENT ID YOU WANT TO DELETE - "))
-
-        print("[+] PATIENT SUCCESSFULLY DELETED !")
 
     def update_patient(self):
         print("[+] UPDATING PATIENT....")
-        self.display_data()
-        patient_id = int(input("ENTER PATIENT ID YOU WANT TO UPDATE - "))
-        patient_field = input("ENTER PATIENT INFORMATION FIELD YOU WANT TO UPDATE - ").lower()
-        for patient in self.patient_data["patient_list"]:
-            if patient["patient_id"] == patient_id:
-                patient[patient_field] = input("ENTER VALUE - ")
-        print("[+] PATIENT SUCCESSFULLY UPDATED !")
-
-        self.save_data()
-        print("[+] SAVED FILE !\n")
-
 
 class Appointment:
     choice = 0
@@ -92,14 +72,12 @@ class Appointment:
 def main_menu():
     choice = int(input(("1. Patient Menu\n2. Appointment Menu\nChoice - ")))
     if choice == 1:
-        #p_file_name = input("ENTER PATIENT FILE NAME") + ".json"
-        p_file_name = "patient_data.json"
+        p_file_name = input("ENTER PATIENT FILE NAME") + ".json"
         patient_data = open(p_file_name, "r")
         patient1 = Patient(patient_data)
         patient1.patient_menu()
     elif choice == 2:
-        #ap_file_name = input("ENTER APPOINTMENT FILE NAME (.json) - ")
-        ap_file_name = "patient_data.json"
+        ap_file_name = input("ENTER APPOINTMENT FILE NAME")
         appointment_data = open(ap_file_name, "r")
         appointment1 = Appointment(appointment_data)
         appointment1.appointment_menu()
