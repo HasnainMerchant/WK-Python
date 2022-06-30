bc9d420069ba4786ec917a19fe381e2b93cd220c
Hasnain Merchant <hasnainmerchant0@gmail.com>
2022-06-30 11:32:21 +0530

    Iterator Example

---
+++ iterators.py
@@ -0,0 +1,14 @@
+import json
+
+path = "JSON_CSV/"
+
+json_file = open(path+"patient_data.json")
+json_data = json.load(json_file)
+p_iterable = json_data["patient_list"]
+p_itr = iter(p_iterable)
+
+while True:
+    try:
+        print(next(p_itr))
+    except StopIteration:
+        print("List Has No More Items")
